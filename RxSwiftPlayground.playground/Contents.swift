fc1aa11e8e8e7e1e10b56b36af13678082818f04
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-29 21:20:48 +0200

    Disposing and Terminating

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -34,3 +34,31 @@ observable3.subscribe { (event) in
     }
 }
 
+// Disposing and Terminating
+print("---------Disposing_and_Terminating--------")
+print("---------First_Exp--------")
+let disposeBag = DisposeBag()
+Observable.of("A","B","C","D","E")
+    .subscribe {
+        print($0)
+}.disposed(by: disposeBag)
+
+print("---------Second_Exp--------")
+
+Observable<String>.create { observer in
+    
+    observer.onNext("A")
+    observer.onCompleted()
+    observer.onNext("?")
+    
+    return Disposables.create()
+}.subscribe(onNext: {
+    print($0)
+}, onError: {
+     print($0)
+}, onCompleted: {
+    print("Completed")
+}) {
+    print("Disposed")
+}
+.disposed(by: disposeBag)
