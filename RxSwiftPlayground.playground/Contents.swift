02f5bb08735584fcb5903f56a97585af0026dde3
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-29 17:10:30 +0200

    Implementing Subscriptions

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -5,9 +5,32 @@ let observable = Observable.just(1)
 
 // Create array with different argument
 let observable2 = Observable.of(1,2,3,4)
-
-let observable3 = Observable.of([1,2,3,4])
-
+//Create observable on whole array
+let observable3 = Observable.of([1,2,3,4,5,6])
+//Create observable on individual element of array
 let observable4 = Observable.from([1,2,3,4])
 
+print("-----------observable4_Example_1-------------")
+observable4.subscribe { (event) in
+       print("event4 is: \(event)")
+    if let element = event.element{
+
+        print("element4 is: \(element)")
+
+    }
+}
+print("--------observable4_Example_2_ With_onNext--------")
+observable4.subscribe(onNext: { (element) in
+    print("element4 is: \(element)")
+})
+    
+print("-----------observable3-------------")
+observable3.subscribe { (event) in
+       print("event3 is: \(event)")
+    if let element = event.element{
+
+        print("element3 is: \(element)")
+
+    }
+}
 
